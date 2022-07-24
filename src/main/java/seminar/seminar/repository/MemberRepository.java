0376c68f4474c6831ee7751ad61cbe986d58fabc
younlll <youn.lee96@gmail.com>
2022-07-24 12:13:09 +0900

    chore: 미사용 코드 삭제

--- src/main/java/seminar/seminar/repository/MemberRepository.java
+++ src/main/java/seminar/seminar/repository/MemberRepository.java
@@ -1,9 +1,7 @@
 package seminar.seminar.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import seminar.seminar.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    List<Member> findAllByIdentificationCode(String identificationCode);
 }
