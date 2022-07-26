01cace88cc401bfcf4a64bb44d1513b46b5d862c
younlll <youn.lee96@gmail.com>
2022-07-26 16:42:49 +0900

    feat: 필터 요청 변수 수정

--- src/main/java/seminar/seminar/config/JwtAuthenticationFilter.java
+++ src/main/java/seminar/seminar/config/JwtAuthenticationFilter.java
@@ -37,7 +37,6 @@ public class JwtAuthenticationFilter implements Filter {
             validateRoles(request, roles);
             request.setAttribute("email", jwtTokenProvider.getSubject(token));
         }
-        request.setAttribute("email", jwtTokenProvider.getSubject(token));
         chain.doFilter(request, response);
     }
 
